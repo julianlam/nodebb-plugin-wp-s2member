4a1b78287a953ec10d6c49b7765fa81f46ed9b09
Julian Lam <julian@designcreateplay.com>
2016-02-08 11:42:50 -0500

    finishing up PMPro integration

--- library.js
+++ library.js
@@ -1,12 +1,22 @@
 "use strict";
 
 var async = require.main.require('async'),
+	winston = require.main.require('winston'),
+	_ = require.main.require('underscore'),
 	xmlrpc = require('xmlrpc');
 
-var db = module.parent.require('./database');
+var db = module.parent.require('./database'),
+	groups = module.parent.require('./groups'),
+	meta = module.parent.require('./meta');
 
 var controllers = require('./lib/controllers'),
-	plugin = {};
+	plugin = {
+		_settings: {
+			host: '127.0.0.1',
+			port: 80,
+			path: '/xmlrpc.php'
+		}
+	};
 
 plugin.init = function(params, callback) {
 	var router = params.router,
@@ -16,7 +26,14 @@ plugin.init = function(params, callback) {
 	router.get('/admin/plugins/wp-paidmembershipspro', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
 	router.get('/api/admin/plugins/wp-paidmembershipspro', controllers.renderAdminPage);
 
-	callback();
+	async.waterfall([
+		async.apply(plugin.refreshSettings),
+		function(next) {
+			plugin.client = xmlrpc.createClient({ host: plugin._settings.host, port: plugin._settings.port, path: plugin._settings.path });
+			next();
+		},
+		async.apply(plugin.createGroups)
+	], callback);
 };
 
 plugin.addAdminNavigation = function(header, callback) {
@@ -29,19 +46,53 @@ plugin.addAdminNavigation = function(header, callback) {
 	callback(null, header);
 };
 
+plugin.refreshSettings = function(callback) {
+	meta.settings.get('wp-paidmembershipspro', function(err, settings) {
+		plugin._settings = _.defaults(settings, plugin._settings);
+		callback();
+	});
+};
+
+plugin.createGroups = function(callback) {
+	/*
+		Note: Can't use this method as the pmp plugin returns boolean even if I pass in true, 'true', 1, or '1' to hasMembershipAccess.
+	*/
+
+	// winston.verbose('[plugins/wp-paidmembershipspro] Querying for membership levels...');
+	// plugin.client.methodCall('pmpro.hasMembershipAccess', [plugin._settings.adminuser, plugin._settings.adminpass, 1, 1, 'true'], function(err, payload) {
+	// 	if (err) {
+	// 		winston.error('[plugins/wp-paidmembershipspro] Unable to retrieve groups list!');
+	// 		return callback();
+	// 	}
+
+	// 	console.log(payload);
+		callback();
+	// });
+};
+
 plugin.process = function(data, callback) {
-	var client = xmlrpc.createClient({ host: 'somehost', port: 80, path: '/test/xmlrpc.php' });
-
-	client.methodCall('pmpro.getMembershipLevelForUser', ['adminusername', 'adminpassword', data.profile.ID], function(err, value) {
-		console.log(err, value);
-	});
-
-	db.getObjectField(data.strategy.name + 'Id:uid', data.profile.ID, function(err, uid) {
+	plugin.client.methodCall('pmpro.getMembershipLevelForUser', [plugin._settings.adminuser, plugin._settings.adminpass, parseInt(data.profile.ID, 10)], function(err, membership) {
 		if (err) {
-			return callback(err);
+			winston.error('[plugins/wp-paidmembershipspro] Unable to verify user membership data for uid ' + data.user.uid + ', revoking...');
+			return plugin.revokeAccess(data.uid, callback);
 		}
-		callback(null, uid);
+
+		plugin.grantAccess(data.user.uid, membership.name, callback);
 	});
+};
+
+plugin.grantAccess = function(uid, groupName, callback) {
+	winston.verbose('[plugins/wp-paidmembershipspro] Granting access to "' + groupName + '" for uid ' + uid);
+	async.series([
+		async.apply(groups.create, { name: groupName, hidden: 1 }),
+		async.apply(groups.join, groupName, 1),	// Put the admin user in this group too
+		async.apply(groups.join, groupName, uid)
+	], callback);
+};
+
+plugin.revokeAccess = function(uid, callback) {
+	winston.verbose('[plugins/wp-paidmembershipspro] Revoking access to membership groups for uid ' + uid);
+	callback();
 };
 
 module.exports = plugin;
--- static/templates/admin/plugins/wp-paidmembershipspro.tpl
+++ static/templates/admin/plugins/wp-paidmembershipspro.tpl
@@ -4,7 +4,7 @@
 		<div class="col-sm-10 col-xs-12">
 			<p class="lead">
 				In order to properly connect with Paid Memberships Pro, we&apos;ll need access to your
-				administrative account.
+				administrative account, as well as requisite XML-RPC connection details.
 			</p>
 			<div class="form-group">
 				<label for="adminuser">Username</label>
@@ -12,7 +12,19 @@
 			</div>
 			<div class="form-group">
 				<label for="adminpass">Password</label>
-				<input type="text" id="adminpass" name="adminpass" title="Password" class="form-control" placeholder="Password">
+				<input type="password" id="adminpass" name="adminpass" title="Password" class="form-control" placeholder="Password">
+			</div>
+			<div class="form-group">
+				<label for="host">XML-RPC Host</label>
+				<input type="text" id="host" name="host" title="XML-RPC Host" class="form-control" placeholder="example.org">
+			</div>
+			<div class="form-group">
+				<label for="port">XML-RPC Port</label>
+				<input type="text" id="port" name="port" title="XML-RPC Port" class="form-control" placeholder="80">
+			</div>
+			<div class="form-group">
+				<label for="path">XML-RPC Path</label>
+				<input type="text" id="path" name="path" title="XML-RPC Path" class="form-control" placeholder="/xmlrpc.php">
 			</div>
 		</div>
 	</div>
